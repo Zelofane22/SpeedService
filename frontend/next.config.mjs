c1daceee98fdac23a285b47077e8bf9e9765bc06
Zelofane22 <fouadechitou@gmail.com>
2026-06-26 08:25:08 +0200

    refactor: remove standalone output configuration from next.config files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,5 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../'),
 }
 
 export default nextConfig
